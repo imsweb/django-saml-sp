8e244f535649e630ff98505abc8b40d67f55015e
Dan Watson <watsond@imsweb.com>
2019-11-19 10:01:03 -0500

    Move setuptools metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,6 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-app = __import__("sp")
+# Note, to build a wheel that does not include testapp (bdist_wheel does not respect find/exclude):
+# python setup.py sdist && pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz
 
-setup(
-    name="django-saml-sp",
-    version=app.__version__,
-    description="A Django application for running one or more SAML service providers (SP).",
-    author="Dan Watson",
-    author_email="watsond@imsweb.com",
-    url="https://github.com/imsweb/django-saml-sp",
-    license="BSD",
-    packages=find_packages(exclude=["testapp", "testapp.*"]),
-    install_requires=["cryptography", "python3-saml",],
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-    project_urls={"Source": "https://github.com/imsweb/django-saml-sp"},
-)
+setup()
