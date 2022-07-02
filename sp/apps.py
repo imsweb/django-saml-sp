d9f10040a812d88338f1fc85982e40f8b45d32a0
Job Doesburg <job.doesburg@gmail.com>
2022-07-02 14:43:05 +0200

    Set default_auto_field on AppConfig

--- sp/apps.py
+++ sp/apps.py
@@ -5,3 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class SPConfig(AppConfig):
     name = "sp"
     verbose_name = _("SAML SP")
+    default_auto_field = "django.db.models.AutoField"
+
